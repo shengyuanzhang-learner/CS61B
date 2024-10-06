0a1c9e31bb660c2210789d57f0f4a0922ce686f6
zsy <1748264392@sjtu.edu.cn>
2024-10-06 21:48:16 +0800

    lab07

---
+++ lab07/src/RedBlackTree.java
@@ -0,0 +1,171 @@
+public class RedBlackTree<T extends Comparable<T>> {
+
+    /* Root of the tree. */
+    RBTreeNode<T> root;
+
+    static class RBTreeNode<T> {
+
+        final T item;
+        boolean isBlack;
+        RBTreeNode<T> left;
+        RBTreeNode<T> right;
+
+        /**
+         * Creates a RBTreeNode with item ITEM and color depending on ISBLACK
+         * value.
+         * @param isBlack
+         * @param item
+         */
+        RBTreeNode(boolean isBlack, T item) {
+            this(isBlack, item, null, null);
+        }
+
+        /**
+         * Creates a RBTreeNode with item ITEM, color depending on ISBLACK
+         * value, left child LEFT, and right child RIGHT.
+         * @param isBlack
+         * @param item
+         * @param left
+         * @param right
+         */
+        RBTreeNode(boolean isBlack, T item, RBTreeNode<T> left,
+                   RBTreeNode<T> right) {
+            this.isBlack = isBlack;
+            this.item = item;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /**
+     * Creates an empty RedBlackTree.
+     */
+    public RedBlackTree() {
+        root = null;
+    }
+
+    /**
+     * Flips the color of node and its children. Assume that NODE has both left
+     * and right children
+     * @param node
+     */
+    void flipColors(RBTreeNode<T> node) {
+        node.isBlack = false;
+        node.left.isBlack = true;
+        node.right.isBlack = true;
+    }
+
+    /**
+     * Rotates the given node to the right. Returns the new root node of
+     * this subtree. For this implementation, make sure to swap the colors
+     * of the new root and the old root!
+     * @param node
+     * @return
+     */
+    RBTreeNode<T> rotateRight(RBTreeNode<T> node) {
+        RBTreeNode<T> leftNode = node.left;
+        boolean nodeColor = node.isBlack;
+        boolean nodeLeftColor = leftNode.isBlack;
+        leftNode.isBlack = nodeColor;
+        node.isBlack = nodeLeftColor;
+        RBTreeNode<T> originalRightNodeOfLeftNode = leftNode.right;
+        leftNode.right = node;
+        node.left = originalRightNodeOfLeftNode;
+        return leftNode;
+    }
+
+    /**
+     * Rotates the given node to the left. Returns the new root node of
+     * this subtree. For this implementation, make sure to swap the colors
+     * of the new root and the old root!
+     * @param node
+     * @return
+     */
+    RBTreeNode<T> rotateLeft(RBTreeNode<T> node) {
+        RBTreeNode<T> rightNode = node.right;
+        boolean nodeColor = node.isBlack;
+        boolean nodeRightColor = rightNode.isBlack;
+        rightNode.isBlack = nodeColor;
+        node.isBlack = nodeRightColor;
+        RBTreeNode<T> originalLeftNodeOfRightNode = rightNode.left;
+        rightNode.left = node;
+        node.right = originalLeftNodeOfRightNode;
+        return rightNode;
+    }
+
+
+    /**
+     * Helper method that returns whether the given node is red. Null nodes (children or leaf
+     * nodes) are automatically considered black.
+     * @param node
+     * @return
+     */
+    private boolean isRed(RBTreeNode<T> node) {
+        return node != null && !node.isBlack;
+    }
+
+    /**
+     * Inserts the item into the Red Black Tree. Colors the root of the tree black.
+     * @param item
+     */
+    public void insert(T item) {
+        root = insert(root, item);
+        root.isBlack = true;
+    }
+
+    /**
+     * Inserts the given node into this Red Black Tree. Comments have been provided to help break
+     * down the problem. For each case, consider the scenario needed to perform those operations.
+     * Make sure to also review the other methods in this class!
+     * @param node
+     * @param item
+     * @return
+     */
+    private RBTreeNode<T> insert(RBTreeNode<T> node, T item) {
+        if (node == null) {
+            // A new red node is created when adding a new item
+            return new RBTreeNode<>(false, item, null, null); // Newly inserted node is red (false)
+        }
+
+        int compare = item.compareTo(node.item);
+
+        // If the item is less than the current node's item, go left
+        if (compare < 0) {
+            node.left = insert(node.left, item);
+        } else if (compare > 0) {
+            // If the item is greater, go right
+            node.right = insert(node.right, item);
+        } else {
+            // If the item already exists, return the node (do nothing)
+            return node;
+        }
+
+        // Fix the tree properties after insertion
+        // Check if the current node violates the red-black properties
+        if (isRed(node.right) && !isRed(node.left)) {
+            node = rotateLeft(node);
+        }
+        if (isRed(node.left) && isRed(node.left.left)) {
+            node = rotateRight(node);
+        }
+        if (isRed(node.left) && isRed(node.right)) {
+            flipColors(node);
+        }
+
+        return node;
+
+        // TODO: Insert (return) new red leaf node.
+
+
+        // TODO: Handle normal binary search tree insertion.
+
+        // TODO: Rotate left operation
+
+        // TODO: Rotate right operation
+
+        // TODO: Color flip
+
+        //fix this return statement
+    }
+
+}
